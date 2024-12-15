a8f6ab9bfd954b95fdbb98e6ae9167315cee6485
HalfMatt <89921672+HalfMatt@users.noreply.github.com>
2024-12-16 00:06:17 +0800

    Installed and configured Tailwind CSS, please run npm install again

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -23,7 +23,7 @@ function App(): JSX.Element {
         </div>
         <div className="action">
           <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
+            Send ping
           </a>
         </div>
       </div>
